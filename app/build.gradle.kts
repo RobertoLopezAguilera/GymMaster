a72b0b1206ca3a433be6ca87a6efb601c8ac6ca1
Roberto <s19120168@alumnos.itsur.edu.mx>
2025-07-14 09:30:01 -0600

    Android 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gimnasio"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
